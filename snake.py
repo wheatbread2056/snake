f35124f6e06c9c7bea4fc66ff805133fc410d2ee
wheatbread2056 <geometrydashiscool222@gmail.com>
2024-05-28 18:58:51 -0700

    basic movement

--- snake.py
+++ snake.py
@@ -1,21 +1,18 @@
-"""
-SNAKE IN PYTHON
-made by wheatbred#0000
-runs in terminal at ~30fps
-"""
+# SNAKE IN PYTHON!!!
 
 def printf(str): # render part of level
-    print(str, end=' ')
+    print(str, end='')
 
 # IMPORTS
 import random as r;
 import time as t;
 import os as o;
+import keyboard as k;
 
 # GAME SETUP
 grid = 0;
-gs = (0,0);
-FPS = 30;
+gs = [0,0];
+FPS = 5;
 
 def gen_grid(x,y):
     global grid, gs;
@@ -27,24 +24,27 @@ def gen_grid(x,y):
             a.append(0);
         grid.append(a);
 
-snakepos = (0,0);
+snakepos = {
+    'x': 0,
+    'y': 0
+}
 snakedir = 0 # 1 = left, 2 = right, 3 = up, 4 = down
 snaketrail = [];
 snakesize = 1;
 
 def spawnSnake():
     global snakepos, snakedir;
-    snakepos = (int(gs[0]/2),int(gs[1]/2));
+    snakepos['x'] = int(gs[0]/2); snakepos['y'] = int(gs[1]/2)
     snakedir = 0;
 
-def render(clear=1):
+def render(clear=1): # render the level
     global grid;
     if clear == 1:
         o.system('cls')
     for i in range(gs[1]): # y coordinate
         for j in range(gs[0]): # x coordinate
             SnakeHead = 0; SnakeTrail = 0
-            if snakepos[0] == j and snakepos[1] == i: # render snake head
+            if snakepos['x'] == j and snakepos['y'] == i: # render snake head
                 printf('Q')
                 SnakeHead = 1
             if SnakeHead == 0 and SnakeTrail == 0:
@@ -59,8 +59,34 @@ def render(clear=1):
                     printf('.');
         print() # newline
 
-gen_grid(10,10)
+def movement(): # move the player
+    # 1 = left, 2 = right, 3 = up, 4 = down
+    global snakepos, snakedir;
+    if snakedir == 1:
+        snakepos['x'] -= 1
+    elif snakedir == 2:
+        snakepos['x'] += 1
+    elif snakedir == 3:
+        snakepos['y'] -= 1
+    elif snakedir == 4:
+        snakepos['y'] += 1
+
+def controller(): # control the player
+    # 1 = left, 2 = right, 3 = up, 4 = down
+    global snakedir;
+    if k.is_pressed('w') or k.is_pressed('up'):
+        snakedir = 3
+    elif k.is_pressed('a') or k.is_pressed('left'):
+        snakedir = 1
+    elif k.is_pressed('s') or k.is_pressed('down'):
+        snakedir = 4
+    elif k.is_pressed('d') or k.is_pressed('right'):
+        snakedir = 2
+
+gen_grid(50,40)
 spawnSnake()
 while True:
     render()
+    movement()
+    controller()
     t.sleep(1/FPS)
